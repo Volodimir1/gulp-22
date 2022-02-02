5febc47318fca84b7f03b4d71c0a61046ffb5636
Volodimir1 <vladimirn062012@gmail.com>
2022-02-02 23:51:36 +0200

    g9

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -1,6 +1,10 @@
 import replace from "gulp-replace"; // Search and replace
+import plumber from "gulp-plumber"; // Error processing
+import notify from "gulp-notify"; // Messages (tips)
 
 // Export object
 export const plugins = {
-   replace: replace
+   replace: replace,
+   plumber: plumber,
+   notify: notify
 }
