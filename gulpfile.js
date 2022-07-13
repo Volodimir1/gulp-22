cd1e3f063562414c7fe54069e2b41e6c2386900b
Volodimir1 <vladimirn062012@gmail.com>
2022-07-14 00:53:07 +0300

    g16

--- gulpfile.js
+++ gulpfile.js
@@ -21,6 +21,7 @@ import { scss } from "./gulp/tasks/scss.js";
 import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
+import { svgSprive } from "./gulp/tasks/svgSprive.js";
 
 // File change watcher
 function watcher() {
@@ -30,6 +31,8 @@ function watcher() {
    gulp.watch(path.watch.js, js);
    gulp.watch(path.watch.images, images);
 }
+
+export { svgSprive }
 
 // Consistent fonts handling
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
